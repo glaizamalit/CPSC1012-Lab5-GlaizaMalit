908511a99739835d95fe6f6afeeaa8ed59a207f0
Glaiza Loren Malit <122337351+gmalit@users.noreply.github.com>
2024-01-09 15:03:59 -0700

    Add project files.

---
+++ CPSC1012-Lab5-GlaizaMalit/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+
+const string FilePath = @"C:\TextFiles\Marks.txt";
+double[] marks = new double[25];
+int markCount;
+
+markCount = LoadData(FilePath, marks);
+PrintSummary(marks, markCount);
+
+
+static int LoadData(string filePath, double[] marks)
+{
+    int numberOfMarks = 0;
+    try
+    {
+        var lines = File.ReadAllLines(filePath);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            marks[i] = double.Parse(lines[i]);
+            numberOfMarks++;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error reading from {filePath} with exception {ex.Message}");
+    }
+
+    return numberOfMarks - 1;
+}
+
+static void PrintSummary(double[] marks, int markCount)
+{
+    double percentage,
+           totalPercentage = 0;
+    int numberOfPass = 0,
+        numberOfFail = 0;
+    Console.WriteLine($"\nQuiz marks: Quiz Total = {marks[0]}");
+    Console.WriteLine($"Mark\tPercentage");
+    
+    for (int i = 1; i <= markCount; i++)
+    {
+        percentage = marks[i] / marks[0];
+        totalPercentage += percentage;
+        if ((percentage * 100) >= 50)
+        {
+            numberOfPass++;
+        }
+        else
+        {
+            numberOfFail++;
+        }
+        Console.WriteLine($"{marks[i],5:F1}\t{percentage,8:P2}");
+    }
+    Console.WriteLine($"The class average is {(totalPercentage / markCount):P2}");
+    Console.WriteLine($"There were {numberOfPass} passes and {numberOfFail} fails.");
+}
